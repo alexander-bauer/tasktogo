b49d838e3ec7f4bd91d5a09ac3d9a9addfd121ff
Sasha Crofter <sasha@crofter.org>
2013-10-16 11:27:36 -0400

    Implement functioning "add" command

--- command.go
+++ command.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/golang/glog"
 	"io"
+	"strconv"
+	"strings"
+	"time"
 )
 
 import ()
+
+const (
+	DueFormat = "Jan _2 15:04"
+)
 
 type Command struct {
 	// Run is the function underlying the Command, and can be called
@@ -25,6 +33,8 @@ var RunMap = map[string]Runner{
 	"h":    (*Command).CmdHelp,
 	"list": (*Command).CmdList,
 	"l":    (*Command).CmdList,
+	"add":  (*Command).CmdAdd,
+	"a":    (*Command).CmdAdd,
 }
 
 type Runner func(*Command, *Context) error
@@ -33,8 +43,9 @@ func (c *Command) CmdHelp(ctx *Context) (err error) {
 	glog.V(2).Infoln("User invoked help")
 
 	fmt.Fprintf(ctx.Output, "TaskToDo version %s\n\n", Version)
-	fmt.Fprintf(ctx.Output, "    help\t\t\t- print this menu\n")
-	fmt.Fprintf(ctx.Output, "    list\t\t\t- list all tasks\n")
+	fmt.Fprintf(ctx.Output, "    help\t\t\t\t\t- print this menu\n")
+	fmt.Fprintf(ctx.Output, "    list\t\t\t\t\t- list all tasks\n")
+	fmt.Fprintf(ctx.Output, "    add [Task name] [priority] [Month day hour:minute]\t- add a task\n")
 
 	return nil
 }
@@ -52,5 +63,46 @@ func (c *Command) CmdList(ctx *Context) (err error) {
 		}
 	}
 
+	return nil
+}
+
+func (c *Command) CmdAdd(ctx *Context) (err error) {
+	glog.V(2).Infoln("User invoked add")
+
+	t := &Task{}
+	var datestring string
+	// Separate the arguments into sections and fill out the Task with
+	// them. The syntax is "add [multiword name] [priority] [month-day
+	// hour:minute]".
+	for _, arg := range c.Args {
+		// If the Priority has not yet been filled out, try to parse
+		// the current argument as an int. Otherwise, append the
+		// argument to the datestring to be parsed at the end.
+		if t.Priority == 0 {
+			t.Priority, err = strconv.Atoi(arg)
+			if err != nil {
+				// If the priority couldn't be parsed, consider it
+				// part of the name.
+				t.Name += arg + " "
+			}
+		} else {
+			datestring += arg + " "
+		}
+	}
+	t.Name = strings.TrimRight(t.Name, " ")
+
+	due, err := time.ParseInLocation(DueFormat,
+		strings.TrimRight(datestring, " "), time.Local)
+	if err != nil {
+		return errors.New("Could not parse arguments")
+	}
+
+	t.DueBy = due.Local()
+
+	// TODO: retrieve a description somehow
+
+	// Now, add the task to the list and set the "modified" flag.
+	ctx.List = append(ctx.List, t)
+	ctx.modified = true
 	return nil
 }
